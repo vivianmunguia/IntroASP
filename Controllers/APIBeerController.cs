a91067c560ad614b2351c72ff101fbe3ae489037
vivianmunguia <andy_munguia@hotmail.com>
2023-10-14 12:15:34 -0700

    Creation of a project that adds beers and brands

---
+++ Controllers/APIBeerController.cs
@@ -0,0 +1,27 @@
+﻿using IntroASP.Models;
+using IntroASP.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIBeerController : ControllerBase
+    {
+        private readonly PubContext _context;
+        public APIBeerController(PubContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<BeerBrandViewModel>> Get()
+        => await _context.Beers.Include(b=>b.Brand)
+            .Select(b=>new BeerBrandViewModel
+            {
+                Name = b.Name,
+                Brand = b.Brand.Name
+            })
+            .ToListAsync();
+    }
+}
---
+++ Models/ViewModels/BeerBrandViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IntroASP.Models.ViewModels
+{
+    public class BeerBrandViewModel
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
